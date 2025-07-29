ed3682ac88f83174357677246f209eb04ed3912a
yuaaiyua <yuaaiyua@proton.me>
2025-07-29 12:09:11 +0700

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ function App() {
   return (
     <>
       <div>
+        <h1 className="text-3xl font-bold underline">    Hello world!  </h1>
+
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
